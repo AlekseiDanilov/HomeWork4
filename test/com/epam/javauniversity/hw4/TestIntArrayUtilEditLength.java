a740f33440f45ded410bd1aa5b6aa7a7d7c432e0
Aleksei Danilov <danilow.aleksei@gmail.com>
2015-11-01 17:23:22 +0300

    add test for method editLength

---
+++ test/com/epam/javauniversity/hw4/TestIntArrayUtilEditLength.java
@@ -0,0 +1,21 @@
+package com.epam.javauniversity.hw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestIntArrayUtilEditLength {
+    @Test
+    public void testIntArrayUtilEditLengthSub() {
+        assertArrayEquals(IntArrayUtil.editLength(new int[]{1, 2, 3, 4, 5}, 3), new int[]{1, 2, 3});
+    }
+    @Test
+    public void testIntArrayUtilEditLengthAdd() {
+        assertArrayEquals(IntArrayUtil.editLength(new int[]{1, 2, 3}, 5), new int[]{1, 2, 3, 0, 0});
+    }
+    @Test
+    public void testIntArrayUtilEditLengthNull() {
+        assertArrayEquals(IntArrayUtil.editLength(null, 5), new int[]{0, 0, 0, 0, 0});
+    }
+}
