e762a5410156d3544b3ac53e991a806a6074ac44
Aleksei Danilov <danilow.aleksei@gmail.com>
2015-11-01 17:51:20 +0300

    add test for method equals

---
+++ test/com/epam/javauniversity/hw4/TestIntArrayUtilEquals.java
@@ -0,0 +1,20 @@
+package com.epam.javauniversity.hw4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestIntArrayUtilEquals {
+    @Test
+    public void testIntArrayUtilEqualsTrue() {
+        assertTrue(IntArrayUtil.equals(new int[]{1, 2, 3}, new int[]{1, 2, 3}));
+    }
+    @Test
+    public void testIntArrayUtilEqualsFalse() {
+        assertFalse(IntArrayUtil.equals(new int[]{1, 2, 3}, new int[]{1, 2, 4}));
+    }
+    @Test
+    public void testIntArrayUtilEqualsNull() {
+        assertFalse(IntArrayUtil.equals(null, new int[]{1, 2, 4}));
+    }
+}
