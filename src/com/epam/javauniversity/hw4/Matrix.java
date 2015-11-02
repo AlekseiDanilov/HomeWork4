a5b752feb9674525be503ea374ca4e2f5cfd2f0b
Aleksei Danilov <danilow.aleksei@gmail.com>
2015-11-02 18:35:49 +0300

    edit constructor, create method isMatrix and test for him

--- src/com/epam/javauniversity/hw4/Matrix.java
+++ src/com/epam/javauniversity/hw4/Matrix.java
@@ -1,61 +1,71 @@
 package com.epam.javauniversity.hw4;
 
 public class Matrix {
-    int[][] matrix;
+    int[][] array;
     int n;
     int m;
 
     public Matrix() {
     }
 
-    public Matrix(int[][] matrix) {
-        this.matrix = matrix;
-        n = numberLine(matrix);
-        m = numberColumns(matrix);
+    public Matrix(int[][] array) {
+        if (isMatrix(array)) {
+            this.array = array;
+            n = numberLine(array);
+            m = numberColumns(array);
+        }
     }
 
     public int getElement(int i, int j) {
         if (i >= n || j >= m || i < 0 || j < 0) {
-            return 0;
+            return 0xFFFF;
         }
-        return matrix[i][j];
+        return array[i][j];
     }
 
     public void setElement(int i, int j, int value) {
         if (i >= n || j >= m || i < 0 || j < 0) {
             return;
         }
-        matrix[i][j] = value;
+        array[i][j] = value;
 
+    }
+
+    public void addition(Matrix matrix) {
+        if (this.n == matrix.n && this.m == matrix.m) {
+            for (int i = 0; i < this.n; i++) {
+                for (int j = 0; j < this.m; j++) {
+                    this.array[i][j] += matrix.array[i][j];
+                }
+            }
+        }
+    }
+    public boolean isMatrix(int[][] array) {
+        if (array == null) {
+            return false;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                return false;
+            }
+        }
+        if (array[0].length == 0) {
+            return false;
+        }
+        int numberElementInLine = array[0].length;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].length != numberElementInLine) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private int numberLine(int[][] matrix) {
-        if (matrix == null) {
-            return 0;
-        }
-        if (matrix.length == 0) {
-            return 0;
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i] == null) {
-                return 0;
-            }
-        }
         return matrix.length;
     }
 
-    private int numberColumns(int[][] matrix) {
-        int numberColumns = 0;
-        if (numberLine(matrix) > 0) {
-            numberColumns = matrix[0].length;
-        } else {
-            return 0;
-        }
-        for (int i = 1; i < numberLine(matrix); i++) {
-            if (matrix[i].length != numberColumns) {
-                return 0;
-            }
-        }
-        return numberColumns;
+    private int numberColumns(int[][] array) {
+        return array[0].length;
     }
 }
---
+++ test/com/epam/javauniversity/hw4/TestMatrixIsMatrix.java
@@ -0,0 +1,32 @@
+package com.epam.javauniversity.hw4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestMatrixIsMatrix {
+    @Test
+    public void testMatrixIsMatrixTrue() {
+        assertTrue(new Matrix().isMatrix(new int[][]{{1, 2}, {3, 4}}));
+    }
+
+    @Test
+    public void testMatrixIsMatrixNull() {
+        assertFalse(new Matrix().isMatrix(null));
+    }
+
+    @Test
+    public void testMatrixIsMatrixFirstLineNull() {
+        assertFalse(new Matrix().isMatrix(new int[][]{null, {1, 2}}));
+    }
+
+    @Test
+    public void testMatrixIsMatrixLineNull() {
+        assertFalse(new Matrix().isMatrix(new int[][]{{1, 2}, null, {5, 9}}));
+    }
+
+    @Test
+    public void testMatrixIsMatrixEmpty() {
+        assertFalse(new Matrix().isMatrix(new int[][]{{}, {}, {}}));
+    }
+}
